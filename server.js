818e9c845c2045705b54999c65dda8bf3aad7241
Hasi <ariyaratha@gmail.com>
2024-11-21 22:34:09 +1100

    added server.js file to support port

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const port = 8080; // Define the custom port here
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
