c4632479b991b6e6a6337562b3ed69788ccd2ea3
Hasi <ariyaratha@gmail.com>
2024-11-22 04:36:37 +1100

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-
+require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
